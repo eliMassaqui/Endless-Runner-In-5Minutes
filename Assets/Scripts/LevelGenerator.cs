420cfdf410914fd6bac153ad8bb4cd0b35fc872c
MELLO <elimassaqui@gmail.com>
2025-06-06 09:51:23 +0100

    Para tornar ainda mais flexível, podes usar uma lista no Inspector (List<GameObject>) para adicionar quantos tiles quiseres sem precisar declarar Tile1, Tile2, Tile3, etc. manualmente.

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
-    public GameObject Tile1;
-    public GameObject Tile2;
+    public List<GameObject> tileOptions; // Agora é uma lista no Inspector
     public GameObject StartTile;
 
     private float Index = 0;
@@ -28,35 +27,15 @@ public class LevelGenerator : MonoBehaviour
     {
         gameObject.transform.position += new Vector3(4 * Time.deltaTime, 0, 0);
 
-        if(transform.position.x >= Index)
+        if (transform.position.x >= Index && tileOptions.Count > 0)
         {
-            int RandomInt1 = Random.Range(0, 2);
+            GameObject TempTile1 = Instantiate(tileOptions[Random.Range(0, tileOptions.Count)], transform);
+            TempTile1.transform.position = new Vector3(-16, 0, 0);
 
-            if(RandomInt1 == 1)
-            {
-                GameObject TempTile1 = Instantiate(Tile1, transform);
-                TempTile1.transform.position = new Vector3(-16, 0, 0);
-            }
-            else if(RandomInt1 == 0)
-            {
-                GameObject TempTile1 = Instantiate(Tile2, transform);
-                TempTile1.transform.position = new Vector3(-16, 0, 0);
-            }
+            GameObject TempTile2 = Instantiate(tileOptions[Random.Range(0, tileOptions.Count)], transform);
+            TempTile2.transform.position = new Vector3(-24, 0, 0);
 
-            int RandomInt2 = Random.Range(0, 2);
-
-            if(RandomInt2 == 1)
-            {
-                GameObject TempTile2 = Instantiate(Tile1, transform);
-                TempTile2.transform.position = new Vector3(-24, 0, 0);
-            }
-            else if(RandomInt2 == 0)
-            {
-                GameObject TempTile2 = Instantiate(Tile2, transform);
-                TempTile2.transform.position = new Vector3(-24, 0, 0);
-            }
-
-            Index = Index + 15.95f;
+            Index += 15.95f;
         }
     }
 }
